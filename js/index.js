753e52868efecbab70626b1b3e9ca9da9bcba8a2
taniryla <ryland0000@gmail.com>
2021-07-05 05:52:41 -0600

    started and halfway through the project

--- js/index.js
+++ js/index.js
@@ -38,5 +38,59 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+// Nav
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+const navEl1 = document.querySelectorAll('nav');
+navEl1.href = siteContent["nav"]["nav-item-1"];
+
+// Top Section
+
+const ctaImg = document.querySelector('.cta img');
+ctaImg.src = './img/header-img.png';
+
+const headline = document.querySelector('.cta-text h1');
+headline.textContent = 'DOM IS AWESOME';
+
+const buttonName = document.querySelector('.cta-text button');
+buttonName.textContent = 'Get Started';
+
+
+// Above the Mid-Photo Content
+
+const textContentEl = document.querySelector('.text-content h4:nth-child(1)');
+textContentEl.textContent = 'Features';
+
+
+const writtenContent = document.querySelector('.text-content p');
+writtenContent.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const topContent = document.querySelector('.top-content');
+topContent.style.display = 'flex';
+topContent.style.flexWrap = 'wrap';
+topContent.style.width = '100%';
+
+// const secondContent = document.querySelector('.text-content h4:nth-child(2)');
+// secondContent.textContent = 'About';
+
+// Below the Mid-Photo Content 
+
+const middleImg = document.querySelector('.main-content img');
+middleImg.src = './img/mid-page-accent.jpg';
+
+// const thirdContent = document.querySelector('.text-content h4:nth-child(3)');
+// thirdContent.textContent = 'Services';
+
+
+// Contact Section 
+
+const contactInfo = document.querySelector('.contact');
+contactInfo.setAttribute('h4', siteContent["contact"]["contact-h4"]);
+contactInfo.setAttribute('p:nth-child(1)', siteContent["contact"]["address"]);
+contactInfo.setAttribute('p:nth-child(2)', siteContent["contact"]["phone"]);
+contactInfo.setAttribute('p:nth-child(3)', siteContent["contact"]["email"]);
